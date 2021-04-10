06f5c92010b9ecd66a8772557d3bc3cf33c33531
MrKira128 <ahmedkira00@gmail.com>
2021-04-10 12:25:08 +0200

    upload the game

---
+++ Floppy-Bird/sketch.js
@@ -0,0 +1,116 @@
+var bird;
+var pipes = [];
+var points = 0;
+function setup() {
+  createCanvas(400, 400);
+  bird = new Bird();
+  pipes.push(new Pipe());
+
+}
+
+function draw() {
+  background(0);
+  
+  if(frameCount%120==0){
+    pipes.push(new Pipe());
+  }
+  for(var i=pipes.length-1;i>=0;i--){
+    pipes[i].update();
+    pipes[i].show();
+
+    if(pipes[i].hits(bird)){
+        console.log("HIT");
+    }
+
+    if(pipes[i].offScreen()){
+        points++;
+        pipes.splice(i,1);
+    }
+  }
+  bird.update();
+  bird.show();
+}
+
+
+
+
+
+
+
+
+function Bird(){
+  this.y = height/2;
+  this.x =64;
+  this.gravity=0.6;
+  this.force = -15;
+  this.velocity =0 ;
+  
+  this.show = ()=>{
+    fill(255);
+    ellipse(this.x,this.y,32,32);
+    
+  }
+  this.up = ()=>{
+    this.velocity+=this.force;
+  }
+  this.update = ()=>{
+    this.velocity+=this.gravity;
+    this.velocity*=0.9;
+    this.y+=this.velocity;
+    
+    if(this.y > height) {
+      this.y = height;
+      this.velocity=0;
+    }
+    if(this.y < 0) {
+      this.y = 0;
+      this.velocity=0;
+    }
+  
+  }
+}
+function Pipe(){
+    this.top=random(height/2);
+    this.bottom=random(height/2);
+    this.x=width;
+    this.w = 20;
+    this.speed = 1;
+    this.highlight = false;
+
+    this.show=()=>{
+        fill(255);
+        if(this.highlight){
+            fill(255,0,0);
+        }
+        rect(this.x,0,this.w,this.top);
+        rect(this.x,height-this.bottom,this.w,this.bottom);
+        
+    }
+    this.update=()=>{
+        this.x-=this.speed;
+    }
+    this.offScreen=()=>{
+        if(this.x<-this.w){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    this.hits=(bird)=>{
+        if((bird.y<this.top) || (bird.y>height-this.bottom)){
+            if((bird.x>this.x) && (bird.x<this.x+this.w)){
+                this.highlight = true;
+                
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
+function keyPressed(){
+  if (key== ' '){
+    bird.up();
+  }
+}
