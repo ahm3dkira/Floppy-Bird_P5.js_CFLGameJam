0babac9bd0f6f749151713c3504152ce15787914
MrKira128 <ahmedkira00@gmail.com>
2021-04-10 13:00:23 +0200

    make it better

--- Floppy-Bird/sketch.js
+++ Floppy-Bird/sketch.js
@@ -6,7 +6,7 @@ function setup() {
   bird = new Bird();
   pipes.push(new Pipe());
 
-}
+} // main setup
 
 function draw() {
   background(0);
@@ -29,14 +29,7 @@ function draw() {
   }
   bird.update();
   bird.show();
-}
-
-
-
-
-
-
-
+} // main draw
 
 function Bird(){
   this.y = height/2;
@@ -68,49 +61,51 @@ function Bird(){
     }
   
   }
-}
+} // Bird
+
 function Pipe(){
-    this.top=random(height/2);
-    this.bottom=random(height/2);
-    this.x=width;
-    this.w = 20;
-    this.speed = 1;
-    this.highlight = false;
+  this.top=random(height/2);
+  this.bottom=random(height/2);
+  this.x=width;
+  this.w = 20;
+  this.speed = 1;
+  this.highlight = false;
 
-    this.show=()=>{
-        fill(255);
-        if(this.highlight){
-            fill(255,0,0);
-        }
-        rect(this.x,0,this.w,this.top);
-        rect(this.x,height-this.bottom,this.w,this.bottom);
-        
-    }
-    this.update=()=>{
-        this.x-=this.speed;
-    }
-    this.offScreen=()=>{
-        if(this.x<-this.w){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    this.hits=(bird)=>{
-        if((bird.y<this.top) || (bird.y>height-this.bottom)){
-            if((bird.x>this.x) && (bird.x<this.x+this.w)){
-                this.highlight = true;
-                
-                return true;
-            }
-        }
+  this.show=()=>{
+      fill(255);
+      if(this.highlight){
+          fill(255,0,0);
+      }
+      rect(this.x,0,this.w,this.top);
+      rect(this.x,height-this.bottom,this.w,this.bottom);
+      
+  }
+  this.update=()=>{
+      this.x-=this.speed;
+  }
+  this.offScreen=()=>{
+      if(this.x<-this.w){
+          return true;
+      }else{
+          return false;
+      }
+  }
+  this.hits=(bird)=>{
+      if((bird.y<this.top) || (bird.y>height-this.bottom)){
+          if((bird.x>this.x) && (bird.x<this.x+this.w)){
+              this.highlight = true;
+              
+              return true;
+          }
+      }
 
-        return false;
+      return false;
 
-    }
-}
+  }
+} // Pipe
+
 function keyPressed(){
   if (key== ' '){
     bird.up();
   }
-}
+} // keyPressed
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Floppy-Bird_P5.js_CFLGameJam
+
+### Notes
+- this game was made using P5.js
